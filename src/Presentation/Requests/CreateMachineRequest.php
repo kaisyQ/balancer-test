10ef725e79bb1676314a53254406e629e4341d65
kaisyQ <85626015+kaisyQ@users.noreply.github.com>
2024-08-19 07:26:26 +0300

    asserts

--- src/Presentation/Requests/CreateMachineRequest.php
+++ src/Presentation/Requests/CreateMachineRequest.php
@@ -5,18 +5,18 @@ namespace App\Presentation\Requests;
 use App\Application\Models\MachineModel;
 use AutoMapper\Attribute\MapTo;
 
+use Symfony\Component\Validator\Constraints as Assert;
 final readonly class CreateMachineRequest 
 {
-
-
-    /**
-     * @TODO добавить Assert
-     */
     public function __construct(
         #[MapTo(target: MachineModel::class, property: 'totalProcess')]
+        #[Assert\NotBlank]
+        #[Assert\NotNull]
         public int $totalProcess,
     
         #[MapTo(target: MachineModel::class, property: 'totalMemory')]
+        #[Assert\NotBlank]
+        #[Assert\NotNull]
         public int $totalMemory
     ){}
 }
--- src/Presentation/Requests/CreateProcessRequest.php
+++ src/Presentation/Requests/CreateProcessRequest.php
@@ -5,17 +5,19 @@ namespace App\Presentation\Requests;
 
 use App\Application\Models\ProcessModel;
 use AutoMapper\Attribute\MapTo;
+use Symfony\Component\Validator\Constraints as Assert;
 
 final readonly class CreateProcessRequest 
 {
-    /**
-     * @TODO добавить Assert
-     */
     public function __construct(
         #[MapTo(target: ProcessModel::class, property: 'totalProcess')]
+        #[Assert\NotBlank]
+        #[Assert\NotNull]
         public int $totalProcess,
     
         #[MapTo(target: ProcessModel::class, property: 'totalMemory')]
+        #[Assert\NotBlank]
+        #[Assert\NotNull]
         public int $totalMemory
     ){}
 }
